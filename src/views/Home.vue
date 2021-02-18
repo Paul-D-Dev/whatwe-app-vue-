68c96541247d8d17d9c613dd262d9a3485c7469e
Paul DAO <dao.van.paul@gmail.com>
2021-02-18 13:18:55 +0100

    CHORES : Remove Card component and set in Home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,34 @@
 <template>
   <div class="home">
-    <Card title="Qu'est-ce qu'on fait" :cards="cardEvents" />
+      <h1>{{title}}</h1>
+      
+      <div class="cards">
+        <ul>
+            <li class="card" v-for="card in cardEvents" :key="card.id">
+                <router-link class="card-link" :to="{name: 'Events', params: {idEvent : card.id}}">
+                    <div class="card-img-wrapper">
+                        <img :src="'http://localhost:1337' + card.image?.url" :alt="card.name">
+                    </div>
+                    <span class="card-name">{{card.name}}</span>
+                </router-link>
+            </li>
+        </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import Card from '@/components/Card.vue'; // @ is an alias to /src
+import { Vue } from 'vue-class-component';
 import EventService from '@/shared/_services/event.service';
 import axios from 'axios';
 
   const eventService = new EventService();
   eventService.fetch('/events');
 
-@Options({
-  components: {
-    Card
-  },
-})
 
 export default class Home extends Vue {
 
+  title = "Qu'est ce qu'on fait"
   cardEvents = [];
 
     mounted () {
@@ -41,5 +49,55 @@ export default class Home extends Vue {
   .home {
     padding-left: 30px;
     padding-right: 30px;
+     $size: 300px;
+
+    ul {
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+        width: 100%;
+        list-style: none;
+        .card {
+            display: flex;
+            flex-direction: column;
+            place-content: center;
+            height: $size;
+            width: $size;
+            margin-bottom: 25px;
+            border-radius: 30px;
+            background-color: rgba(45, 131, 245, 0.45);
+            cursor: pointer;
+            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.01);
+            transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+
+            &:hover {
+                box-shadow: 0 8px 20px rgba(0, 0, 0, 0.10), 0 0px 0px rgba(0, 0, 0, 0.1);
+            }
+
+            .card-link{
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                text-decoration: none;
+                color:#707070;
+
+                .card-img-wrapper {
+                    height: $size/2;
+                    width: $size/2;
+
+                    img {
+                        width: 100%;
+                        height: 100%;
+                        border-radius: $size/4;
+                    }
+                }
+                .card-name {
+                    margin-top: 10px;
+                    text-transform: uppercase;
+                }
+            }
+            
+        }
+    }
   }
 </style>
